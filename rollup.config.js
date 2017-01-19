da5c55490afcf02e4dae043271caf6a03f834a2b
Hendrik Richert <hendrik.richert@swisscom.com>
2017-01-19 13:39:17 +0100

    provide backwards-compatibility for pre-es2015 browsers via babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-//import babel from 'rollup-plugin-babel';
+import babel from 'rollup-plugin-babel';
 
 export default {
     format: 'umd',
     entry: 'js/index.js',
     dest: 'dist/batterylevel.js',
     sourceMap: true,
-    sourceMapFile: 'dist/batterylevel.js.map'
-    //plugins: [ babel() ]
+    sourceMapFile: 'dist/batterylevel.js.map',
+    plugins: [ babel() ]
 };
